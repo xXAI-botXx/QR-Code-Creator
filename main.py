179bb18d91bb825ae03d1c0bfa89cf62e3fb76fa
Tobia <tobia2761@gmail.com>
2022-06-14 19:46:46 +0200

    Added QR-Code Reader + added control for creation

---
+++ main.py
@@ -0,0 +1,129 @@
+import qr_code_creator
+import sys
+from ast import literal_eval
+
+def exit():
+    # delete output and input files -> or remember user
+    # ...
+    # now exit
+    sys.exit()
+
+def get_input(msg:str):
+    user_input = input(msg).lower()
+
+    if user_input == "exit":
+        exit()
+    else:
+        return user_input
+
+def get_input_between(msg, min, max, type_):
+    wrong_input = True
+
+    while wrong_input:
+        user_input = get_input(msg)
+        try:
+            value = type_(user_input)
+            if value >= min and value <= max:
+                wrong_input = False
+        except ValueError:
+            pass
+
+    return value
+
+def get_input_color(msg:str):
+    not_a_color = True
+
+    while not_a_color:
+        user_input = get_input(msg)
+        if not (user_input.startswith("(") or user_input.startswith("[")):
+            user_input = f"({user_input})"
+
+        if not ("," in user_input):
+            user_input = user_input.replace(" ", ",")
+
+        try:
+            color = literal_eval(user_input)
+            if type(color) == tuple or type(color) == list:
+                check = True
+                for i in color:
+                    if i <= 255 and i >= 0:
+                        check = False
+                if check:
+                    not_a_color = False
+        except ValueError:
+            pass
+    return color
+        
+
+    return user_input
+
+def get_input_key(msg:str, keys:list):
+    user_input = get_input(msg)
+
+    while user_input not in keys:
+        user_input = get_input(msg)
+
+    return user_input
+
+def change_and_show(qr_code):
+    while True:
+        print("\nYou find the QR-Code in the output directory.\n")
+        qr_code.create_qr_code()
+        qr_code.save("./output")
+
+        msg = "Do you want to change the style? [size, fill_color, back_color, box_size, style, no]:"
+        choice = get_input_key(msg, ['size', 'fill_color', 'back_color', 'box_size', 'type', 'color mask', 'use image', 'no'])
+        if choice == 'n':
+            print("Nice, your QR-Code is in the ouput directory.")
+            break
+        elif choice == 'size':
+            msg = "Type a number from 1 to 40:"
+            val = get_input_between(msg, 1, 40, int)
+            qr_code.set_size(val)
+        elif choice in ['box_size', 'box size', 'box-size']:
+            msg = "Type a number bigger than 1:"
+            val = get_input_between(msg, 1, 10000000, int)
+            qr_code.set_box_size(val)
+        elif choice == 'fill_color':
+            msg = "Type a color with 3 values (rgb), like (255, 255, 255):"
+            val = get_input_color(msg)
+            qr_code.set_fill_color(val)
+        elif choice == 'back_color':
+            msg = "Type a color with 3 values (rgb), like (255, 255, 255):"
+            val = get_input_color(msg)
+            qr_code.set_background_color(val)
+        elif choice == 'type':
+            msg = "Type square, rounded, circle, gapped square, vertical bar or horizontal bar:"
+            val = get_input_between(msg, ['off', 'square', 'rounded', 'circle', 'gapped square', 'vertical bar', 'horizontal bar'])
+            qr_code.set_type(val)
+        elif choice == 'mask':
+            msg = "Type :"
+            mask_val = get_input_key(msg, ["solid fill", "square gradient", "radial gradient"\
+                                            "horizontal gradient", "vertical gradient"])
+
+            msg = "Type a color for front with 3 values (rgb), normal is (255, 255, 255):"
+            color_front_val = get_input_color(msg)
+
+            msg = "Type a color for background with 3 values (rgb), normal is (0,0,0)):"
+            color_back_val = get_input_color(msg)
+
+            qr_code.set_mask(mask_val, color_front_val, color_back_val)
+        elif choice == 'use image':
+            msg = "Upload an image in the input directory with named as input. Which type is it (png/...):"
+            val = get_input(msg)
+            qr_code.set_image_back(img_type=val)
+
+        
+        qr_code.save(path="./output")
+
+
+if __name__ == "__main__":
+    msg = "Do you want to create a QR-Code or read one? Type create or read:"
+    choice = get_input_key(msg, ['create', 'read'])
+
+    if choice == 'create':
+        content = get_input("Type the Content which the QR-Code should contains:")
+        qr_code = qr_code_creator.QR_Code(content)
+        change_and_show(qr_code)
+    elif 'read':
+        pass
---
+++ qr_code_creator.py
@@ -0,0 +1,118 @@
+import qrcode
+import cv2
+
+# for styles
+from qrcode.image.styledpil import StyledPilImage
+
+from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
+from qrcode.image.styles.moduledrawers import SquareModuleDrawer
+from qrcode.image.styles.moduledrawers import GappedSquareModuleDrawer
+from qrcode.image.styles.moduledrawers import CircleModuleDrawer
+from qrcode.image.styles.moduledrawers import VerticalBarsDrawer
+from qrcode.image.styles.moduledrawers import HorizontalBarsDrawer
+
+from qrcode.image.styles.colormasks import SolidFillColorMask
+from qrcode.image.styles.colormasks import SquareGradiantColorMask
+from qrcode.image.styles.colormasks import RadialGradiantColorMask
+from qrcode.image.styles.colormasks import HorizontalGradiantColorMask
+from qrcode.image.styles.colormasks import VerticalGradiantColorMask
+#from qrcode.image.styles.colormasks import ImageColorMask
+
+
+class QR_Code(object):
+
+    def __init__(self, content:str, fill_color='black', background_color='white', border=4, size=1, box_size=10, \
+                                    draw_type=SquareModuleDrawer, mask=SolidFillColorMask(front_color=(0,0,0), back_color=(255,255,255)), img_back=False):
+        if type(content) == str and len(content) > 0:
+            self.content = content
+            self.fill_color = fill_color
+            self.background_color = background_color
+            self.border = border
+            self.size = size
+            self.box_size = box_size
+            self.draw_type = draw_type
+            self.mask = mask
+            self.img_back = img_back
+
+            self.img_type = "jpg"
+        else:
+            raise ValueError("The given content isn't valuable for the QR-Code!")
+
+    def set_fill_color(self, fill_color):
+        self.fill_color = fill_color
+
+    def set_backgound_color(self, background_color):
+        self.background_color = background_color
+
+    def set_border(self, border):
+        self.border = border
+        self.create_qr_code()
+
+    def set_size(self, size):
+        self.size = size
+        self.create_qr_code()
+
+    def set_box_size(self, box_size):
+        self.box_size = box_size
+        self.create_qr_code()
+
+    def set_color_mask(self, mask, front_color=(0,0,0), back_color=(255,255,255)):
+        if mask == 'off':
+            self.mask = SolidFillColorMask(front_color=(0,0,0), back_color=(255,255,255))
+        elif mask == "solid fill":
+            self.mask = SolidFillColorMask(front_color=front_color, back_color=back_color)
+        elif mask == "square gradient":
+            self.mask = SquareGradiantColorMask(front_color=front_color, back_color=back_color)
+        elif mask == "radial gradient":
+            self.mask = RadialGradiantColorMask(front_color=front_color, back_color=back_color)
+        elif mask == "horizontal gradient":
+            self.mask = HorizontalGradiantColorMask(front_color=front_color, back_color=back_color)
+        elif mask == "vertical gradient":
+            self.mask = VerticalGradiantColorMask(front_color=front_color, back_color=back_color)
+        #elif mask == "image":
+        #    self.mask = ImageColorMask(front_color=front_color, back_color=back_color)
+
+    def set_image_back(self, img_type="jpg"):
+        self.img_type = img_type
+        self.img_back = True
+
+    def set_draw_type(self, draw_type:str):
+        if draw_type == "square":
+            self.draw_type = SquareModuleDrawer()
+        elif draw_type == "rounded":
+            self.draw_type = RoundedModuleDrawer()
+        elif draw_type in ["gapped square", "gapped_square"]:
+            self.draw_type = GappedSquareModuleDrawer()
+        elif draw_type == "circle":
+            self.draw_type = CircleModuleDrawer()
+        elif draw_type == "vertical bar":
+            self.draw_type = VerticalBarsDrawer()
+        elif draw_type == "horizontal bar":
+            self.draw_type = HorizontalBarsDrawer()
+
+    def set_content(content):
+        self.content = content
+        self.create_qr_code()
+
+    def create_qr_code(self):
+        self.created_qr_code = qrcode.QRCode(border=self.border, version=self.size, box_size=self.box_size)
+        self.created_qr_code.add_data(self.content)
+        self.created_qr_code.make(fit=True)
+
+    def save(self, path:str, name="output.png", img_path="./input", img_name="input", img_type=self.img_type):
+        if self.img_back == True:
+            self.img = qr.make_image(fill_color=self.fill_color, back_color=self.background_color, image_factory=StyledPilImage, color_mask=self.mask, \
+                                                                    module_drawer=self.draw_type, embeded_image_path=img_path+"/"+img_name+"."+img_type)
+        else:
+           self.img = qr.make_image(fill_color=self.fill_color, back_color=self.background_color, image_factory=StyledPilImage, color_mask=self.mask, \
+                                                                    module_drawer=self.draw_type) 
+
+        self.img.save(path+"/"+name)
+
+    # static
+    def get_content_from_qr_code(path_with_filename:str):
+        qr_reader = cv2.QRCodeDetector()
+        value, points, qrcode_ = qr_reader.detectAndDecode(cv2.imread(path_with_filename))
+        return value
+
+
