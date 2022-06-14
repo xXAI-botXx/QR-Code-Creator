3b7572cdb8448c7cced883115c8d7f84ffa61d0c
Tobia <tobia2761@gmail.com>
2022-06-14 23:51:12 +0200

    Removed Features + bugs - removed image selection - fixed bug -> forgotten () by class call

--- qr_code_creator.py
+++ qr_code_creator.py
@@ -1,5 +1,5 @@
 import qrcode
-import cv2
+#import cv2
 
 # for styles
 from qrcode.image.styledpil import StyledPilImage
@@ -22,7 +22,7 @@ from qrcode.image.styles.colormasks import VerticalGradiantColorMask
 class QR_Code(object):
 
     def __init__(self, content:str, fill_color='black', background_color='white', border=4, size=1, box_size=10, \
-                                    draw_type=SquareModuleDrawer, mask=SolidFillColorMask(front_color=(0,0,0), back_color=(255,255,255)), img_back=False):
+                                    draw_type=SquareModuleDrawer(), mask=SolidFillColorMask(front_color=(0,0,0), back_color=(255,255,255)), img_back=False):
         if type(content) == str and len(content) > 0:
             self.content = content
             self.fill_color = fill_color
@@ -97,22 +97,22 @@ class QR_Code(object):
     def create_qr_code(self):
         self.created_qr_code = qrcode.QRCode(border=self.border, version=self.size, box_size=self.box_size)
         self.created_qr_code.add_data(self.content)
-        self.created_qr_code.make(fit=True)
+        #self.created_qr_code.make(fit=True)
 
     def save(self, path:str, name="output.png", img_path="./input", img_name="input"):
         if self.img_back == True:
             self.img = self.created_qr_code.make_image(fill_color=self.fill_color, back_color=self.background_color, image_factory=StyledPilImage, color_mask=self.mask, \
                                                                     module_drawer=self.draw_type, embeded_image_path=img_path+"/"+img_name+"."+self.img_type)
         else:
-           self.img = self.created_qr_code.make_image(fill_color=self.fill_color, back_color=self.background_color, image_factory=StyledPilImage, color_mask=self.mask, \
-                                                                    module_drawer=self.draw_type) 
+           self.img = self.created_qr_code.make_image(fill_color=self.fill_color, back_color=self.background_color, image_factory=StyledPilImage, \
+                                                                    module_drawer=self.draw_type, color_mask=self.mask) 
 
         self.img.save(path+"/"+name)
 
     # static
-    def get_content_from_qr_code(path_with_filename:str):
-        qr_reader = cv2.QRCodeDetector()
-        value, points, qrcode_ = qr_reader.detectAndDecode(cv2.imread(path_with_filename))
-        return value
+    #def get_content_from_qr_code(path_with_filename:str):
+    #    qr_reader = cv2.QRCodeDetector()
+    #    value, points, qrcode_ = qr_reader.detectAndDecode(cv2.imread(path_with_filename))
+    #    return value
 
 
